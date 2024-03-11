b7f0fc85ede12b1bdced712d037b08cf09685949
Brandon Munongi <brandon.munongi@gmail.com>
2024-03-11 16:57:40 +0200

    Fixed Bug

--- src/store.js
+++ src/store.js
@@ -1,18 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { thunk } from 'redux-thunk'; // Importing Redux Thunk correctly
+import { thunk } from 'redux-thunk'; // Correct import of Redux Thunk
 import rootReducer from './reducers';
 
 const initialState = {};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middleware = applyMiddleware(thunk);
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    middleware,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(middleware) // Applying the composeEnhancers to the middleware
 );
 
 export default store;
